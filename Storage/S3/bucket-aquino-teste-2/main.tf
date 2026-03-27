2d5b4987a1f36d44801fd90dbbd3764ea5829c82
Gerson Aquino <85296922+gerson-aquino@users.noreply.github.com>
2026-03-27 01:14:54 -0300

    terraform-ai: add main.tf for bucket-aquino-teste-2

---
+++ Storage/S3/bucket-aquino-teste-2/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "aquino-tech"
+    workspaces {
+      name = "bucket-aquino-teste-2-s3-bucket"
+    }
+  }
+}
+
+module "bucket-aquino-teste-2" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.6.0"
+
+  bucket        = var.bucket
+  acl           = var.acl
+  versioning    = var.versioning
+  force_destroy = var.force_destroy
+
+  tags = local.common_tags
+}
