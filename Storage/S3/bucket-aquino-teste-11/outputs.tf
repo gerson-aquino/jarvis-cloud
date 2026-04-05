e321994767a1231e66a7aba8268adbea548315b8
Gerson Aquino <85296922+gerson-aquino@users.noreply.github.com>
2026-04-05 00:23:51 -0300

    terraform-ai: add outputs.tf for bucket-aquino-teste-11

---
+++ Storage/S3/bucket-aquino-teste-11/outputs.tf
@@ -0,0 +1,19 @@
+output "s3_bucket_id" {
+  description = "The S3 bucket ID"
+  value       = module.bucket-aquino-teste-11.s3_bucket_id
+}
+
+output "s3_bucket_arn" {
+  description = "The S3 bucket ARN"
+  value       = module.bucket-aquino-teste-11.s3_bucket_arn
+}
+
+output "s3_bucket_bucket_domain_name" {
+  description = "The S3 bucket domain name"
+  value       = module.bucket-aquino-teste-11.s3_bucket_bucket_domain_name
+}
+
+output "s3_bucket_bucket_regional_domain_name" {
+  description = "The S3 bucket regional domain name"
+  value       = module.bucket-aquino-teste-11.s3_bucket_bucket_regional_domain_name
+}
