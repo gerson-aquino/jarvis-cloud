d605bf249f8ac9c5da78e15492eb9cf8c0c5a9ec
Gerson Aquino <85296922+gerson-aquino@users.noreply.github.com>
2026-04-05 00:23:52 -0300

    terraform-ai: add provider.tf for bucket-aquino-teste-11

---
+++ Storage/S3/bucket-aquino-teste-11/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  cloud {
+    organization = "aquino-tech"
+    workspaces {
+      name = "JARVIS-CLOUD_Storage_S3_bucket-aquino-teste-11"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
