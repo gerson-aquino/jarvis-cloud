82edfe977365549b99e98af2fc971b081f383536
Gerson Aquino <85296922+gerson-aquino@users.noreply.github.com>
2026-04-05 01:07:23 -0300

    terraform-ai: add tags.tf for bucket-aquino-teste-33

---
+++ Storage/S3/bucket-aquino-teste-33/tags.tf
@@ -0,0 +1,12 @@
+module "resource_tags" {
+  source = "git@github.com:Superlogica/terraform-aws-modules.git//tags?ref=tags-v0.0.5"
+
+  environment      = "hml"
+  cost_center      = "PG12334445"
+  area             = "teste"
+  is_shared        = false
+  custom_tags      = {}
+  tag_organization = "superlogica"
+  tag_workload     = "superlogica"
+  tag_billing      = "superlogica"
+}
